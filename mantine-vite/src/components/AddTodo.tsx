5a9713f2cca4395bcb518fd4d748fd217af381f0
Indra Ikhwal <indra@iglobal.co.id>
2022-11-03 23:15:18 +0700

    First Commit

---
+++ mantine-vite/src/components/AddTodo.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { useForm } from '@mantine/form';
+import { Modal, Button, Group, TextInput, Textarea } from '@mantine/core';
+import { ENDPOINT } from '../App';
+import { KeyedMutator } from 'swr';
+
+function AddTodo({ mutate }: { mutate: KeyedMutator<Todo[]> }) {
+  const [open, setOpen] = useState(false)
+
+  const form = useForm({
+    initialValues: {
+      title: "",
+      body: "",
+    }
+  })
+
+  async function createTodo(values: { title: string, body: string }) {
+    const updated = await fetch(`${ENDPOINT}/api/todos`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values),
+    }).then((r) => { r.json });
+
+    mutate(updated)
+    form.reset()
+    setOpen(false)
+  }
+
+
+
+  return (<>
+    <Modal opened={open} onClose={() => setOpen(false)} title="Create todo">
+      <form onSubmit={form.onSubmit(createTodo)}>
+        <TextInput required mb={12} label="Todo" placeholder="Masukan todo disini" {...form.getInputProps("title")}></TextInput>
+        <Textarea required mb={12} label="body" placeholder="Detail todo" {...form.getInputProps("body")}></Textarea>
+
+        <Button type="submit">Create todo</Button>
+      </form>
+    </Modal>
+    <Group position="center">
+      <Button fullWidth mb={12} onClick={() => setOpen(true)}>Add Todo</Button>
+    </Group>
+  </>);
+}
+
+export default AddTodo
